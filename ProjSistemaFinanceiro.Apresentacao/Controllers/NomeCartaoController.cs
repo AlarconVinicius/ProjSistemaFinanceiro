e54dc05642d1662652a67f5647d76e4a9f92322f
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-15 10:28:16 -0300

    Mapeamento NomeCartao

--- ProjSistemaFinanceiro.Apresentacao/Controllers/NomeCartaoController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/NomeCartaoController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.NomeCartao;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IServicos;
 using ProjSistemaFinanceiro.Dominio.Servicos;
@@ -13,30 +15,36 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
     public class NomeCartaoController : ControllerBase
     {
         private readonly INomeCartaoService _iNomeCartaoService;
+        private readonly IMapper _mapper;
 
-        public NomeCartaoController(INomeCartaoService iNomeCartaoService)
+        public NomeCartaoController(INomeCartaoService iNomeCartaoService, IMapper mapper)
         {
             _iNomeCartaoService = iNomeCartaoService;
+            _mapper = mapper;
         }
 
         [HttpPost]
-        public async Task AdicionarNomeCartao(NomeCartaoEntity objeto)
+        public async Task AdicionarNomeCartao(NomeCartaoAddDTO objeto)
         {
-            //var objetoMapeado = _mapper.Map<NomeCartao>(objeto);
-            await _iNomeCartaoService.AdicionarNomeCartao(objeto);
+            var objetoMapeado = _mapper.Map<NomeCartaoEntity>(objeto);
+            await _iNomeCartaoService.AdicionarNomeCartao(objetoMapeado);
         }
 
         [HttpGet]
-        public async Task<ResultadoPagina<NomeCartaoEntity>> ListarNomeCartoes([FromQuery] Guid? nomeCartaoId = null)
+        public async Task<ResultadoPagina<NomeCartaoViewDTO>> ListarNomeCartoes([FromQuery] Guid? nomeCartaoId = null)
         {
             var objeto = await _iNomeCartaoService.ListarNomeCartoes(nomeCartaoId);
-            //var objetoMapeado = _mapper.Map<List<NomeCartaoView>>(objeto);
-            return objeto;
+            var objetoMapeado = _mapper.Map<List<NomeCartaoViewDTO>>(objeto);
+            return new ResultadoPagina<NomeCartaoViewDTO>
+            {
+                Resultado = objetoMapeado
+            };
         }
         [HttpPut]
-        public async Task AtualizarNomeCartao(NomeCartaoEntity objeto)
+        public async Task AtualizarNomeCartao(NomeCartaoUpdDTO objeto)
         {
-            await _iNomeCartaoService.AtualizarNomeCartao(objeto);
+            var objetoMapeado = _mapper.Map<NomeCartaoEntity>(objeto);
+            await _iNomeCartaoService.AtualizarNomeCartao(objetoMapeado);
         }
         [HttpDelete]
         public async Task DeletarNomeCartao([FromQuery] Guid id)
