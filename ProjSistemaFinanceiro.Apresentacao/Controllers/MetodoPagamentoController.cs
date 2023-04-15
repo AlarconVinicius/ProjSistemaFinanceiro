c13968de5e8acbee8145ef68f3b23587e36ba219
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-15 10:28:05 -0300

    Mapeamento MetodoPagamento

--- ProjSistemaFinanceiro.Apresentacao/Controllers/MetodoPagamentoController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/MetodoPagamentoController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Banco;
+using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.MetodoPagamento;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IServicos;
 using ProjSistemaFinanceiro.Dominio.Servicos;
 using ProjSistemaFinanceiro.Entidade.Entidades;
@@ -12,29 +15,36 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
     public class MetodoPagamentoController : ControllerBase
     {
         private readonly IMetodoPagamentoService _iMetodoPagamentoService;
+        private readonly IMapper _mapper;
 
-        public MetodoPagamentoController(IMetodoPagamentoService iMetodoPagamentoService)
+        public MetodoPagamentoController(IMetodoPagamentoService iMetodoPagamentoService, IMapper mapper)
         {
             _iMetodoPagamentoService = iMetodoPagamentoService;
+            _mapper = mapper;
         }
 
         [HttpPost]
-        public async Task AdicionarMetodoPagamento(MetodoPagamentoEntity objeto)
+        public async Task AdicionarMetodoPagamento(MetodoPagamentoAddDTO objeto)
         {
-            await _iMetodoPagamentoService.AdicionarMetodoPagamento(objeto);
+            var objetoMapeado = _mapper.Map<MetodoPagamentoEntity>(objeto);
+            await _iMetodoPagamentoService.AdicionarMetodoPagamento(objetoMapeado);
         }
 
         [HttpGet]
-        public async Task<ResultadoPagina<MetodoPagamentoEntity>> ListarMetodosPagamento([FromQuery] Guid? metodoPagamentoId = null)
+        public async Task<ResultadoPagina<MetodoPagamentoViewDTO>> ListarMetodosPagamento([FromQuery] Guid? metodoPagamentoId = null)
         {
             var objeto = await _iMetodoPagamentoService.ListarMetodosPagamento(metodoPagamentoId);
-            //var objetoMapeado = _mapper.Map<List<MetodoPagamentoView>>(objeto);
-            return objeto;
+            var objetoMapeado = _mapper.Map<List<MetodoPagamentoViewDTO>>(objeto.Resultado);
+            return new ResultadoPagina<MetodoPagamentoViewDTO>
+            {
+                Resultado = objetoMapeado
+            };
         }
         [HttpPut]
-        public async Task AtualizarMetodoPagamento(MetodoPagamentoEntity objeto)
+        public async Task AtualizarMetodoPagamento(MetodoPagamentoUpdDTO objeto)
         {
-            await _iMetodoPagamentoService.AtualizarMetodoPagamento(objeto);
+            var objetoMapeado = _mapper.Map<MetodoPagamentoEntity>(objeto);
+            await _iMetodoPagamentoService.AtualizarMetodoPagamento(objetoMapeado);
         }
         [HttpDelete]
         public async Task DeletarMetodoPagamento([FromQuery] Guid id)
