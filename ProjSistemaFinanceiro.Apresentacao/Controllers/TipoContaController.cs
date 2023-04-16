7e1b71d7799940f8727583ffccbffc4a68fc7c05
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-16 14:56:17 -0300

    Mapeamento TipoConta

--- ProjSistemaFinanceiro.Apresentacao/Controllers/TipoContaController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/TipoContaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.TipoConta;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
