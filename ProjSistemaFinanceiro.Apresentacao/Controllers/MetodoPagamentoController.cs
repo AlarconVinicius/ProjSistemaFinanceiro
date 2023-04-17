91e4a916222e4fa6e43c237d6f8ea35de7244a7c
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-17 15:06:54 -0300

    Implementando Validação MetodoPagamentoController

--- ProjSistemaFinanceiro.Apresentacao/Controllers/MetodoPagamentoController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/MetodoPagamentoController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Banco;
+using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Categoria;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.MetodoPagamento;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IServicos;
 using ProjSistemaFinanceiro.Dominio.Servicos;
@@ -16,18 +18,38 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
     {
         private readonly IMetodoPagamentoService _iMetodoPagamentoService;
         private readonly IMapper _mapper;
+        private IValidator<MetodoPagamentoAddDTO> _addValidator;
+        private IValidator<MetodoPagamentoUpdDTO> _updValidator;
 
-        public MetodoPagamentoController(IMetodoPagamentoService iMetodoPagamentoService, IMapper mapper)
+        public MetodoPagamentoController(IMetodoPagamentoService iMetodoPagamentoService, IMapper mapper, IValidator<MetodoPagamentoAddDTO> addValidator, IValidator<MetodoPagamentoUpdDTO> updValidator)
         {
             _iMetodoPagamentoService = iMetodoPagamentoService;
             _mapper = mapper;
+            _addValidator = addValidator;
+            _updValidator = updValidator;
         }
 
         [HttpPost]
-        public async Task AdicionarMetodoPagamento(MetodoPagamentoAddDTO objeto)
+        public async Task<ActionResult> AdicionarMetodoPagamento(MetodoPagamentoAddDTO objeto)
         {
+            var validacao = await _addValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                    //Erros = result.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<MetodoPagamentoEntity>(objeto);
             await _iMetodoPagamentoService.AdicionarMetodoPagamento(objetoMapeado);
+            return Ok();
         }
 
         [HttpGet]
@@ -37,14 +59,32 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
             var objetoMapeado = _mapper.Map<List<MetodoPagamentoViewDTO>>(objeto.Resultado);
             return new ResultadoPagina<MetodoPagamentoViewDTO>
             {
+                Titulo = "Listagem dos métodos de pagamento.",
+                Status = 200,
                 Resultado = objetoMapeado
             };
         }
         [HttpPut]
-        public async Task AtualizarMetodoPagamento(MetodoPagamentoUpdDTO objeto)
+        public async Task<ActionResult> AtualizarMetodoPagamento(MetodoPagamentoUpdDTO objeto)
         {
+            var validacao = await _updValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                    //Erros = result.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<MetodoPagamentoEntity>(objeto);
             await _iMetodoPagamentoService.AtualizarMetodoPagamento(objetoMapeado);
+            return Ok();
         }
         [HttpDelete]
         public async Task DeletarMetodoPagamento([FromQuery] Guid id)
