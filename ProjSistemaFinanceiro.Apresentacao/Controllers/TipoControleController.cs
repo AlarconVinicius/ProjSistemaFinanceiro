332738d570fe711a3f93d8c781c948efc5ea17f3
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-17 15:14:34 -0300

    Implementando Validação TipoControleController

--- ProjSistemaFinanceiro.Apresentacao/Controllers/TipoControleController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/TipoControleController.cs
@@ -1,10 +1,8 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.TipoControle;
-using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IServicos;
-using ProjSistemaFinanceiro.Dominio.Servicos;
 using ProjSistemaFinanceiro.Entidade.Entidades;
 using ProjSistemaFinanceiro.Entidade.ResultadoPaginas;
 
@@ -16,18 +14,38 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
     {
         private readonly ITipoControleService _iTipoControleService;
         private readonly IMapper _mapper;
+        private IValidator<TipoControleAddDTO> _addValidator;
+        private IValidator<TipoControleUpdDTO> _updValidator;
 
-        public TipoControleController(ITipoControleService iTipoControleService, IMapper mapper)
+        public TipoControleController(ITipoControleService iTipoControleService, IMapper mapper, IValidator<TipoControleAddDTO> addValidator, IValidator<TipoControleUpdDTO> updValidator)
         {
             _iTipoControleService = iTipoControleService;
             _mapper = mapper;
+            _addValidator = addValidator;
+            _updValidator = updValidator;
         }
 
         [HttpPost]
-        public async Task AdicionarTipoControle(TipoControleAddDTO objeto)
+        public async Task<ActionResult> AdicionarTipoControle(TipoControleAddDTO objeto)
         {
+            var validacao = await _addValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                    //Erros = result.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<TipoControleEntity>(objeto);
             await _iTipoControleService.AdicionarTipoControle(objetoMapeado);
+            return Ok();
         }
 
         [HttpGet]
@@ -37,14 +55,32 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
             var objetoMapeado = _mapper.Map<List<TipoControleViewDTO>>(objeto);
             return new ResultadoPagina<TipoControleViewDTO>
             {
+                Titulo = "Listagem dos tipos de controles.",
+                Status = 200,
                 Resultado = objetoMapeado
             };
         }
         [HttpPut]
-        public async Task AtualizarTipoControle(TipoControleUpdDTO objeto)
+        public async Task<ActionResult> AtualizarTipoControle(TipoControleUpdDTO objeto)
         {
+            var validacao = await _updValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                    //Erros = result.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<TipoControleEntity>(objeto);
             await _iTipoControleService.AtualizarTipoControle(objetoMapeado);
+            return Ok();
         }
         [HttpDelete]
         public async Task DeletarTipoControle([FromQuery] Guid id)
