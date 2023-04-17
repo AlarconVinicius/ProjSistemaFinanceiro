82455c4bad3369c1ff8036d3e4ccf6aecfddbcad
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-17 15:04:09 -0300

    Implementando Validação CategoriaController

--- ProjSistemaFinanceiro.Apresentacao/Controllers/CategoriaController.cs
+++ ProjSistemaFinanceiro.Apresentacao/Controllers/CategoriaController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Banco;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Categoria;
 using ProjSistemaFinanceiro.Apresentacao.DTO.DTOs.Transacao;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
@@ -17,18 +19,38 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
     {
         private readonly ICategoriaService _iCategoriaService;
         private readonly IMapper _mapper;
+        private IValidator<CategoriaAddDTO> _addValidator;
+        private IValidator<CategoriaUpdDTO> _updValidator;
 
-        public CategoriaController(ICategoriaService iCategoriaService, IMapper mapper)
+        public CategoriaController(ICategoriaService iCategoriaService, IMapper mapper, IValidator<CategoriaAddDTO> addValidator, IValidator<CategoriaUpdDTO> updValidator)
         {
             _iCategoriaService = iCategoriaService;
             _mapper = mapper;
+            _addValidator = addValidator;
+            _updValidator = updValidator;
         }
 
         [HttpPost]
-        public async Task AdicionarCategoria(CategoriaAddDTO objeto)
+        public async Task<ActionResult> AdicionarCategoria(CategoriaAddDTO objeto)
         {
+            var validacao = await _addValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                    //Erros = result.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<CategoriaEntity>(objeto);
             await _iCategoriaService.AdicionarCategoria(objetoMapeado);
+            return Ok();
         }
 
         [HttpGet]
@@ -38,14 +60,31 @@ namespace ProjSistemaFinanceiro.Apresentacao.Controllers
             var objetoMapeado = _mapper.Map<List<CategoriaViewDTO>>(objeto.Resultado);
             return new ResultadoPagina<CategoriaViewDTO>
             {
+                Titulo = "Listagem das categorias.",
+                Status = 200,
                 Resultado = objetoMapeado
             };
         }
         [HttpPut]
-        public async Task AtualizarCategoria(CategoriaUpdDTO objeto)
+        public async Task<ActionResult> AtualizarCategoria(CategoriaUpdDTO objeto)
         {
+            var validacao = await _updValidator.ValidateAsync(objeto);
+            if (!validacao.IsValid)
+            {
+                var resultado = new ResultadoErroPagina
+                {
+                    Titulo = "Ocorreu um ou mais erros de validação.",
+                    Status = 400,
+                };
+                validacao.Errors.ForEach(error =>
+                {
+                    resultado.AdicionarErro(error.PropertyName, error.ErrorMessage);
+                });
+                return BadRequest(resultado);
+            }
             var objetoMapeado = _mapper.Map<CategoriaEntity>(objeto);
             await _iCategoriaService.AtualizarCategoria(objetoMapeado);
+            return Ok();
         }
         [HttpDelete]
         public async Task DeletarCategoria([FromQuery] Guid id)
