532b8b2866ee18e05e593937b32cdea88e779609
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-20 18:21:30 -0300

    Migration add_usuarioId

--- ProjSistemaFinanceiro.Infraestrutura/Migrations/20230420211217_add_usuarioId.Designer.cs
+++ ProjSistemaFinanceiro.Infraestrutura/Migrations/20230420211217_add_usuarioId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjSistemaFinanceiro.Infraestrutura.Configuracao;
 
@@ -11,9 +12,10 @@ using ProjSistemaFinanceiro.Infraestrutura.Configuracao;
 namespace ProjSistemaFinanceiro.Infraestrutura.Migrations
 {
     [DbContext(typeof(ContextoBase))]
-    partial class ContextoBaseModelSnapshot : ModelSnapshot
+    [Migration("20230420211217_add_usuarioId")]
+    partial class add_usuarioId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjSistemaFinanceiro.Infraestrutura/Migrations/20230420211217_add_usuarioId.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjSistemaFinanceiro.Infraestrutura.Migrations
+{
+    public partial class add_usuarioId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Transacoes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "TipoControles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "TipoContas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "NomeCartoes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "MetodosDePagamentos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Categorias",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioId",
+                table: "Bancos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Transacoes");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "TipoControles");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "TipoContas");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "NomeCartoes");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "MetodosDePagamentos");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Categorias");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Bancos");
+        }
+    }
+}
