875da895388b3c5dca473a6f52e43854b1e38505
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-20 18:21:50 -0300

    Adicionando AutenticacaoHelper

---
+++ ProjSistemaFinanceiro.Identity/Servicos/AutenticacaoHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace ProjSistemaFinanceiro.Identity.Servicos
+{
+    public static class AutenticacaoHelper
+    {
+        public static async Task<string> ObterUsuarioId(IHttpContextAccessor httpContextAccessor)
+        {
+            var user = httpContextAccessor.HttpContext.User;
+
+            if (user?.Identity?.IsAuthenticated == true)
+            {
+                return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+            }
+
+            return null;
+        }
+    }
+
+}
