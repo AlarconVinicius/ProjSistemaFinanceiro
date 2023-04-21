d3488e96255936dadf448f5e41c51a7334ac7730
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-21 10:06:44 -0300

    Adicionando Autenticação em Categoria

--- ProjSistemaFinanceiro.Dominio/Servicos/CategoriaService.cs
+++ ProjSistemaFinanceiro.Dominio/Servicos/CategoriaService.cs
@@ -1,7 +1,9 @@
-﻿using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
+﻿using Microsoft.AspNetCore.Http;
+using ProjSistemaFinanceiro.Dominio.Interfaces.IClasses;
 using ProjSistemaFinanceiro.Dominio.Interfaces.IServicos;
 using ProjSistemaFinanceiro.Entidade.Entidades;
 using ProjSistemaFinanceiro.Entidade.ResultadoPaginas;
+using ProjSistemaFinanceiro.Identity.Servicos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +15,16 @@ namespace ProjSistemaFinanceiro.Dominio.Servicos
     public class CategoriaService : ICategoriaService
     {
         private readonly ICategoria _iCategoria;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CategoriaService(ICategoria iCategoria)
+        public CategoriaService(ICategoria iCategoria, IHttpContextAccessor httpContextAccessor)
         {
             _iCategoria = iCategoria;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task AdicionarCategoria(CategoriaEntity objeto)
         {
+            objeto.UsuarioId = await AutenticacaoHelper.ObterUsuarioId(_httpContextAccessor);
             objeto.DataCriacao = DateTime.Now;
             objeto.DataAlteracao = DateTime.Now;
             await _iCategoria.Adicionar(objeto);
@@ -27,18 +32,35 @@ namespace ProjSistemaFinanceiro.Dominio.Servicos
 
         public async Task AtualizarCategoria(CategoriaEntity objeto)
         {
-            objeto.DataAlteracao = DateTime.Now;
-            await _iCategoria.Atualizar(objeto);
+            var usuarioId = await AutenticacaoHelper.ObterUsuarioId(_httpContextAccessor);
+            var objetoDb = await _iCategoria.ObterPorId(objeto.Id);
+            if (objetoDb.UsuarioId != usuarioId)
+            {
+                throw new Exception("Você não tem permissão para atualizar este registro.");
+            }
+            objetoDb.Nome = objeto.Nome;
+            objetoDb.Descricao = objeto.Descricao;
+            objetoDb.DataAlteracao = DateTime.Now;
+
+            await _iCategoria.Atualizar(objetoDb);
         }
 
         public async Task DeletarCategoria(Guid id)
         {
+            var usuarioId = await AutenticacaoHelper.ObterUsuarioId(_httpContextAccessor);
+            var objetoDb = await _iCategoria.ObterPorId(id);
+            if (objetoDb.UsuarioId != usuarioId)
+            {
+                throw new Exception("Você não tem permissão para deletar este registro.");
+            }
+
             await _iCategoria.Deletar(id);
         }
 
         public async Task<ResultadoPagina<CategoriaEntity>> ListarCategorias(Guid? categoriaId = null)
         {
-            return await _iCategoria.ListarCategorias(categoriaId);
+            var usuarioId = await AutenticacaoHelper.ObterUsuarioId(_httpContextAccessor);
+            return await _iCategoria.ListarCategorias(usuarioId, categoriaId);
         }
     }
 }
