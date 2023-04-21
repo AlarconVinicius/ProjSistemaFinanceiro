7a231280785215e58a40bdf48ae85f9d2a8cbb81
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-21 10:49:22 -0300

    Adicionando Autenticação em Transacao

--- ProjSistemaFinanceiro.Dominio/Interfaces/IClasses/ITransacao.cs
+++ ProjSistemaFinanceiro.Dominio/Interfaces/IClasses/ITransacao.cs
@@ -2,18 +2,12 @@
 using ProjSistemaFinanceiro.Entidade.Entidades;
 using ProjSistemaFinanceiro.Entidade.Filtros;
 using ProjSistemaFinanceiro.Entidade.ResultadoPaginas;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjSistemaFinanceiro.Dominio.Interfaces.IClasses
 {
     public interface ITransacao : IGenerica<TransacaoEntity>
     {
         Task AdicionarTransacoes(List<TransacaoEntity> listaObjeto);
-        Task AtualizarTransacao(TransacaoEntity objeto);
-        Task<ResultadoPagina<TransacaoEntity>> ListarTransacoes(TransacaoFiltro? filtro = null);
+        Task<ResultadoPagina<TransacaoEntity>> ListarTransacoes(string usuarioId, TransacaoFiltro? filtro = null);
     }
 }
