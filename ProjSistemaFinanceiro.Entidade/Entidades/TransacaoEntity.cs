1dab8762d6689b25b7a4b9abf1d214d9e9f6a84c
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-04-27 22:02:26 -0300

    Adicionando Coluna Stutas Transacao

--- ProjSistemaFinanceiro.Entidade/Entidades/TransacaoEntity.cs
+++ ProjSistemaFinanceiro.Entidade/Entidades/TransacaoEntity.cs
@@ -5,6 +5,46 @@ namespace ProjSistemaFinanceiro.Entidade.Entidades
 {
     public class TransacaoEntity : BaseEntity
     {
+        //public TransacaoEntity(string usuarioId, Guid tipoControleId, Guid categoriaId, Guid bancoId, Guid tipoContaId, Guid metodoPagamentoId, Guid nomeCartaoId, string nome, string estabelecimento, double valor, bool entrada, bool pago, DateTime dataCompra, DateTime dataPagamento, string? descricao = "")
+        //{
+        //    UsuarioId = usuarioId;
+        //    TipoControleId = tipoControleId;
+        //    CategoriaId = categoriaId;
+        //    BancoId = bancoId;
+        //    TipoContaId = tipoContaId;
+        //    MetodoPagamentoId = metodoPagamentoId;
+        //    NomeCartaoId = nomeCartaoId;
+        //    Nome = nome;
+        //    Descricao = descricao;
+        //    Estabelecimento = estabelecimento;
+        //    Status = ObterStatus();
+        //    Valor = valor;
+        //    Entrada = entrada;
+        //    Pago = pago;
+        //    DataCompra = dataCompra;
+        //    DataPagamento = dataPagamento;
+        //}
+
+        //public TransacaoEntity(Guid id, string usuarioId, Guid categoriaId, Guid bancoId, Guid tipoContaId, Guid metodoPagamentoId, Guid nomeCartaoId, string nome, string estabelecimento, double valor, bool entrada, bool pago, DateTime dataCompra, DateTime dataPagamento, string? descricao = "")
+        //{
+        //    Id = id;
+        //    UsuarioId = usuarioId;
+        //    CategoriaId = categoriaId;
+        //    BancoId = bancoId;
+        //    TipoContaId = tipoContaId;
+        //    MetodoPagamentoId = metodoPagamentoId;
+        //    NomeCartaoId = nomeCartaoId;
+        //    Nome = nome;
+        //    Descricao = descricao;
+        //    Estabelecimento = estabelecimento;
+        //    Status = ObterStatus();
+        //    Valor = valor;
+        //    Entrada = entrada;
+        //    Pago = pago;
+        //    DataCompra = dataCompra;
+        //    DataPagamento = dataPagamento;
+        //}
+
         [Column(Order = 2)]
         public string UsuarioId { get; set; }
 
@@ -40,6 +80,7 @@ namespace ProjSistemaFinanceiro.Entidade.Entidades
         [StringLength(700)]
         public string? Descricao { get; set; }
         public string Estabelecimento { get; set; }
+        public string? Status { get; set; }
 
         public double Valor { get; set; } = 00.00;
 
@@ -50,5 +91,24 @@ namespace ProjSistemaFinanceiro.Entidade.Entidades
         public DateTime DataPagamento { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataAlteracao { get; set; }
+
+        public string ObterStatus()
+        {
+            if (!this.Pago)
+            {
+                if (this.DataPagamento > DateTime.Today)
+                {
+                    return "Pendente";
+                }
+                else
+                {
+                    return "Vencido";
+                }
+            }
+            else
+            {
+                return "Pago";
+            }
+        }
     }
 }
--- ProjSistemaFinanceiro.Infraestrutura/Migrations/20230427232144_add_coluna_status_transacao.Designer.cs
+++ ProjSistemaFinanceiro.Infraestrutura/Migrations/20230427232144_add_coluna_status_transacao.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjSistemaFinanceiro.Infraestrutura.Configuracao;
 
@@ -11,9 +12,10 @@ using ProjSistemaFinanceiro.Infraestrutura.Configuracao;
 namespace ProjSistemaFinanceiro.Infraestrutura.Migrations
 {
     [DbContext(typeof(ContextoBase))]
-    partial class ContextoBaseModelSnapshot : ModelSnapshot
+    [Migration("20230427232144_add_coluna_status_transacao")]
+    partial class add_coluna_status_transacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
